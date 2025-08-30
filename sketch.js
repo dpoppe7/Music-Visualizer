6b68bde9e614e2f4250b2d109fe077c4eaf9cbb5
Damaris Poppe <damariscecilia29@gmail.com>
2025-08-30 00:36:00 -0600

    Added FFT Audio Analysis class audioManager

--- sketch.js
+++ sketch.js
@@ -4,12 +4,14 @@ class AudioManager{
         this.audio = null;
         this.isLoaded = false;
         this.isPlaying = false;
-        this.amp = new p5.Amplitude(); // analyze volume
+        this.amp = new p5.Amplitude(); // analyze volume. 
+        this.fft = new p5.FFT(0.3, 512); // analysis algorithm that isolates individual audio frequencies within a waveform. Documentation: https://p5js.org/reference/p5.sound/p5.FFT/
     }
     
     loadAudio(file, callback) {
         if (this.audio) this.audio.stop();
             this.audio = loadSound(file, () => {
+                this.fft.setInput(this.audio); // Set the input source for the FFT analysis. If no source is provided, FFT will analyze all sound in the sketch.
                 this.isLoaded = true;
                 console.log('Audio loaded');
                 callback && callback();
@@ -40,8 +42,25 @@ class AudioManager{
     getLevel() {
         return this.isLoaded && this.isPlaying ? this.amp.getLevel() : 0;
     }
-}
 
+    // Returns the amount of energy (volume) at a specific frequency, or the average amount of energy between two frequencies. 
+    // Accepts Number(s) corresponding to frequency (in Hz), or a "string" corresponding to predefined frequency ranges ("bass", "lowMid", "mid", "highMid", "treble")
+    // Documentation: https://p5js.org/reference/p5.sound/p5.FFT/
+    getBassEnergy() {
+        if (!this.isLoaded || !this.isPlaying) return 0;
+        return this.fft.getEnergy("bass");
+    }
+
+    getMidEnergy() {
+        if (!this.isLoaded || !this.isPlaying) return 0;
+        return this.fft.getEnergy("mid");
+    }
+
+    getTrebleEnergy() {
+        if (!this.isLoaded || !this.isPlaying) return 0;
+        return this.fft.getEnergy("treble");
+    }
+}
 //Visual elements for the visualizer: Cards (playing cards like poker :D)
 /*
     GoldenCard represents a single animated card on screen.
